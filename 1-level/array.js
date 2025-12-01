b17cd995d7ee9016ef684cea716e7b33378d6a22
Trofymenko Vladyslav <naughtysora@proton.me>
2025-12-01 23:44:54 +0200

    refined naming, fixed binary search

--- 1-level/array.js
+++ 1-level/array.js
@@ -16,14 +16,14 @@
  * 
  */
 
-function findIndex(target) {
+function linear(target) {
   for (let i = 0; i < this.length; i++) {
     if (this[i] === target) return i;
   }
   return -1;
 }
 
-const there = findIndex.call([1, 2, 3, 4, 5, 6, 7, 8, 9], 4);
+const there = linear.call([1, 2, 3, 4, 5, 6, 7, 8, 9], 4);
 
 /**
  * @description
@@ -37,29 +37,32 @@ const there = findIndex.call([1, 2, 3, 4, 5, 6, 7, 8, 9], 4);
  * Predictable jumps
  * No pointer chasing
  * Even with 1M elements only 20 comparisons
-
+ * 
+ * 0[-----]100
+ * 0---[--]100
+ * 0---|--100
  */
 
-function search(index) {
-  if (index < 0 || index > this.length) return -1;
-  let step = 0;
-  let last = this.length - 1;
-  while (step <= last) {
-    const middle = (step + last) >> 1;
-    if (middle < index) {
-      step = middle + 1;
+function binary(value) {
+  let left = 0;
+  let right = this.length - 1;
+  while (left <= right) {
+    const middle = (left + right) >> 1;
+    const current = this[middle];
+    if (current < value) {
+      left = middle + 1;
       continue;
     }
-    if (middle > index) {
-      last = middle - 1;
+    if (current > value) {
+      right = middle - 1;
       continue;
     }
-    return this[middle];
+    return middle;
   }
   return -1;
 }
 
-const seven = search.call([1, 2, 3, 4, 5, 6, 7, 8, 9], 6);
+const five = binary.call([1, 2, 3, 4, 5, 6, 7, 8, 9], 6);
 
 /**
  * @description
@@ -72,7 +75,7 @@ const seven = search.call([1, 2, 3, 4, 5, 6, 7, 8, 9], 6);
  * Faster than binary search when target is closer to start
  */
 
-function expo(value) {
+function exponential(value) {
   let i = 1;
   const max = this.length - 1;
   while (this[i] < value && i < max) i <<= 1;
@@ -94,8 +97,5 @@ function expo(value) {
   return -1;
 }
 
-
 const array = Array.from({ length: 1000 }, (_, i) => i);
-const s = expo.call(array, 699);
-
-console.log(s);
+const res = exponential.call(array, 699);
