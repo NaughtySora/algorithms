e37d1087ef871f10467b5b029053c8c7542fffa6
Trofymenko Vladyslav <naughtysora@proton.me>
2025-12-20 16:18:20 +0200

    short array algo by categories, export, test linear

--- test/array.js
+++ test/array.js
@@ -2,9 +2,13 @@
 
 const { describe, it } = require('node:test');
 const assert = require('node:assert/strict');
+const { search: { linear } } = require("../lib/array.js");
 
 describe('Array', () => {
-  it('linear', () => {
-
+  describe('search', () => {
+    it('linear', () => {
+      const array = [3, 6, 1, 3, -11, 6];
+      assert.equal(linear.call(array, -11), 4);
+    });
   });
 });
